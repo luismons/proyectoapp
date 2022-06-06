a72489a4e1bf9e62a8d036e50317d87d46cc9802
Luis <luismons15@gmail.com>
2022-06-06 19:39:30 -0400

    modificacion vistas

--- app/src/main/java/com/example/proyectoapp/MainActivity.java
+++ app/src/main/java/com/example/proyectoapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.proyectoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-}
+    public void ingresar(View view){
+        Intent ingresar = new Intent(this,nuevo_contrato.class);
+        startActivity(ingresar);
+    }
+
+
+
+    }
--- app/src/main/java/com/example/proyectoapp/nuevo_contrato.java
+++ app/src/main/java/com/example/proyectoapp/nuevo_contrato.java
@@ -2,7 +2,9 @@ package com.example.proyectoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class nuevo_contrato extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class nuevo_contrato extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_contrato);
+    }
+    public void regresar(View view){
+        Intent regresar = new Intent(this,MainActivity.class);
+        startActivity(regresar);
     }
 }
