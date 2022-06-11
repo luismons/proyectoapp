f95d58216257ac1e748009440ddda2ba297cd6f1
luismons <luismons15@gmail.com>
2022-06-10 20:03:22 -0400

    actualizacion de vistas

---
+++ app/src/main/java/com/example/proyectoapp/registro_conexiones.java
@@ -0,0 +1,14 @@
+package com.example.proyectoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registro_conexiones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_conexiones);
+    }
+}
