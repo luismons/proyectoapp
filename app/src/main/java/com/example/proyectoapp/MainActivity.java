881cf9fffa8bfe178bd478feee095b631243aee1
luismons <luismons15@gmail.com>
2022-06-13 17:16:58 -0400

    login de acceso

--- app/src/main/java/com/example/proyectoapp/MainActivity.java
+++ app/src/main/java/com/example/proyectoapp/MainActivity.java
@@ -9,134 +9,97 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Hashtable;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity{
 
+    String URL_SERVIDOR = "http://192.168.0.101/basedatos/validar_usuario.php";
 
-    private BottomAppBar bottomAppBar;
+    EditText loUsuario, loPassword;
+    Button  btnLogin, btnRegistrar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
-        // Configuración bottomAppBar
-        //
-        bottomAppBar = findViewById(R.id.bottomAppBar);
-        setSupportActionBar(bottomAppBar);
-        configurarBottomAppBar(bottomAppBar);
+        loUsuario=findViewById(R.id.edtUsuario);
+        loPassword=findViewById(R.id.edtPassword);
+        btnRegistrar=findViewById(R.id.btnRegistrar);
+        btnLogin=findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this,registro.class);
+                startActivity(intent);
+            }
+        });
 
     }
-    public void configurarBottomAppBar(BottomAppBar bottomAppBar){
-        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+    public void login() {
+        StringRequest stringRequest;
+        stringRequest = new StringRequest(Request.Method.POST, URL_SERVIDOR, new Response.Listener<String>() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
+            public void onResponse(String response) {
 
-                switch (item.getItemId()){
-                    case R.id.home:
-                        home();
-                        return true;
-                    case R.id.notificar:
-                        notificar();
-                        return true;
-                    case R.id.perfilar:
-                        perfilar();
-                        return true;
-                    case R.id.asignarTurnos:
-                        asignarTurnos();
-                        return true;
-                    case R.id.nuevoContrato:
-                        nuevoContrato();
-                        return true;
-                    case R.id.registroConexion:
-                        registroConexiones();
-                        return true;
-                    case R.id.reporteDigitacion:
-                        reporteDigitacion();
-                        return true;
-                    case R.id.pedidosDigitar:
-                        digitarPedido();
-                        return true;
-                    case R.id.pedidosDigitalizar:
-                        digitalizarPedido();
-                        return true;
-                    case R.id.ingresarBoleta:
-                        ingresarBoleta();
-                        return true;
-                    case R.id.reporteDigitalizacion:
-                        reporteDigitalizacion();
-                        return true;
-                    case R.id.exportar:
-                        exportar();
-                        return true;
-                    default:
-                        return false;
+                if (response.equals("ERROR 1")) {
+                    Toast.makeText(MainActivity.this, "Favor Completar los datos", Toast.LENGTH_SHORT).show();
+                } else if (response.equals("ERROR 2")) {
+                    Toast.makeText(MainActivity.this, "No existe Usuario", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Inicio de sesion exitoso", Toast.LENGTH_SHORT).show();
                 }
 
             }
-        });
-    }
-    public void home(){
-        Intent home = new Intent(this,justaspinner.class);
-        startActivity(home);
-    }
-    public void notificar(){
-        Intent home = new Intent(this,notificar_hhee.class);
-        startActivity(home);
-    }
-    public void perfilar(){
-        Intent home = new Intent(this,perfilar_listausuarios.class);
-        startActivity(home);
-    }
-    public void asignarTurnos(){
-        Intent home = new Intent(this,asignacion_de_turnos.class);
-        startActivity(home);
-    }
-    public void nuevoContrato(){
-        Intent home = new Intent(this,nuevo_contrato.class);
-        startActivity(home);
-    }
-    public void registroConexiones(){
-        Intent home = new Intent(this,registro_conexiones.class);
-        startActivity(home);
-    }
-    public void reporteDigitacion(){
-        Intent home = new Intent(this,reporte_digitacion.class);
-        startActivity(home);
-    }
-    public void digitarPedido(){
-        Intent home = new Intent(this,digitar_pedido.class);
-        startActivity(home);
-    }
-    public void digitalizarPedido(){
-        Intent digitarPedido = new Intent(this,digitalizar_pedido.class);
-        startActivity(digitarPedido);
-    }
-    public void ingresarBoleta(){
-        Intent digitarPedido = new Intent(this,ingresar_boleta.class);
-        startActivity(digitarPedido);
-    }
-    public void reporteDigitalizacion(){
-        Intent digitarPedido = new Intent(this,reporte_digitalizacion.class);
-        startActivity(digitarPedido);
-    }
-    public void exportar(){
-        Intent digitarPedido = new Intent(this,exportar.class);
-        startActivity(digitarPedido);
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "ERROR AL INICIAR SESION", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new Hashtable<String, String>();
+                parametros.put("rut", loUsuario.getText().toString().trim());
+                parametros.put("password", loPassword.getText().toString().trim());
+
+
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(stringRequest);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.bottom_menu, menu);
-        return TRUE;
-    }
 }
